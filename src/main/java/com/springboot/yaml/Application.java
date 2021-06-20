309c9f4853387675dbdc0f13b394afa1528a616e
Dev2 <Dev2@gamil.com>
2021-06-20 13:15:38 +0530

    HIS-301:Related changes

--- src/main/java/com/springboot/yaml/Application.java
+++ src/main/java/com/springboot/yaml/Application.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+        private Map<String,Object> cache=new HashMap<String,Object>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
                 int i=10;
 	}
-
+//HIS-301 changes
+        public void loadDataToCache(){
+        //logic
 }
